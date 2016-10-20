af492c7a6582d140ed045017e27a6781a0b43853
Jim Eastburn <jeastburn@invodo.com>
2016-10-20 14:19:50 -0500

    do gulp build instead of npm build

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -19,7 +19,7 @@ execute "build dist with gulp" do
   cwd release_path
   user "deploy"
   environment "NODE_ENV" => 'production'
-  command "/srv/www/angular2-typescript-gulp/npm run clean & npm run build"
+  command "/srv/www/angular2-typescript-gulp/node_modules/.bin/gulp build"
 end
 
 execute "ensure config directory exists" do
